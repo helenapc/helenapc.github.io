4bf1643dd0933cef61897ffeae32cee4778a5526
helenapc <helenapc2018@gmail.com>
2020-02-17 20:46:11 -0300

    pdate

--- app_p.js
+++ app_p.js
@@ -4,11 +4,11 @@ tit.innerHTML = `<h1>Pablo</h1>`;
 
 const api_url = "https://raw.githubusercontent.com/helenapc/helenapc.github.io/master/test.json"
 
-async function getISS() {
+async function getISS(){
   const response = await fetch(api_url);
   const data = await response.json();
   console.log(data);
-}
+};
 
 getISS();
 
