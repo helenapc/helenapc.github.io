543246ab72aec92a438cc9ee594157fb9baa1f6f
helenapc <58961124+helenapc@users.noreply.github.com>
2020-02-17 20:23:51 -0300

    Update fetch

--- app_p.js
+++ app_p.js
@@ -2,4 +2,14 @@ const tit = document.getElementById("title");
 
 tit.innerHTML = `<h1>Pablo</h1>`;
 
+const api_url = "https://raw.githubusercontent.com/helenapc/helenapc.github.io/master/test.json"
+
+async function getISS() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log(data);
+}
+
+getISS();
+
 console.log('prueba 4');
