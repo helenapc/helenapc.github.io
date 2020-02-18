363bc28eefe3771509bbb4be1051bf44ca09c87a
helenapc <helenapc2018@gmail.com>
2020-02-18 03:16:28 -0300

    rrr

--- app_p.js
+++ app_p.js
@@ -9,8 +9,8 @@ async function getISS(){
   const response = await fetch(api_url);
   const data = await response.json();
   console.log(data);
-  tit.innerHTML += "TEST: " + data.02;
-  txt.innerHTML = "CODE: " + data.05 + "<br/>" + data.06;
+  // tit.innerHTML += "TEST: " + data.02;
+  // txt.innerHTML = "CODE: " + data.05 + "<br/>" + data.06;
 };
 
 
