5aeb08357ef72a4c3f3384c6bb3da2f62cc82962
helenapc <helenapc2018@gmail.com>
2020-02-18 03:32:28 -0300

    123

--- app_p.js
+++ app_p.js
@@ -1,7 +1,7 @@
 const tit = document.getElementById("title");
 const txt = document.getElementById("cod");
 
-tit.innerHTML = `<h1>Pablo</h1>`;
+
 
 const api_url = "https://raw.githubusercontent.com/helenapc/helenapc.github.io/master/test.json"
 
@@ -9,8 +9,8 @@ async function getISS(){
   const response = await fetch(api_url);
   const data = await response.json();
   console.log(data);
-  tit.innerHTML += "TEST: " + data.i2;
-  txt.innerHTML = "CODE: " + data.i5 + "<br/>" + data.i6;
+  tit.innerHTML = `<h1>${data.id1}</h1>`;
+  txt.innerHTML = `CODE: ${data.i5} <br/> DATA: ${data.i6}`;
 };
 
 
