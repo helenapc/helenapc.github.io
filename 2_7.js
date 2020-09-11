3b5269cf4ab473c2dd05f42e616949aede2d5322
helenapc <helenapc2018@gmail.com>
2020-09-11 03:30:00 -0300

    test-update

--- 2_7.js
+++ 2_7.js
@@ -104,7 +104,7 @@ item('barExport', 'arrow-up-circle-outline', 'Crear copia de Seguridad')
 item('barImport', 'arrow-down-circle-outline', 'Cargar copia de Seguridad');
 item('barLogout', 'log-out-outline', 'Cerrar Sesión');
 const ver = document.createElement('ion-item-divider');
-setAttributes(ver, {innerHTML: 'Versión 2.74'});
+setAttributes(ver, {innerHTML: 'Versión 2.74-update'});
 barContent.appendChild(ver);
 item('barDelAcc', 'close-outline', 'Eliminar Cuenta', 'danger');
 
