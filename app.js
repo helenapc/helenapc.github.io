fed328fb2d64e16cd8d437fc85125b0ea403dfce
textoexpositivo <helenapc2018@gmail..com>
2021-01-12 16:55:38 -0300

    2.7.4 -> 2.8 (fix ver)

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ barHeader.appendChild(barToolbar);
 barMenuPrincipal.appendChild(barHeader);
 
 
-const version = 'Versión 2.8-beta';
+const version = 'Versión 2.8';
 itemPers('account', 'person-circle-outline', 'Cuenta');
 itemPers('barExport', 'arrow-up-circle-outline', 'Crear copia de Seguridad');
 itemPers('barImport', 'arrow-down-circle-outline', 'Cargar copia de Seguridad');
